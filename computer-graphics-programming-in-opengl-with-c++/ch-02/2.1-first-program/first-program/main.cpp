6b516b7057494557232fddc3af375d9d9f356c88
Danilo Araújo <daniloaraujo1990@hotmail.com>
2020-07-25 12:31:21 -0300

    Using a simple example shader

--- computer-graphics-programming-in-opengl-with-c++/ch-02/2.1-first-program/first-program/main.cpp
+++ computer-graphics-programming-in-opengl-with-c++/ch-02/2.1-first-program/first-program/main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 using namespace std;
 
-void init(GLFWwindow *window){}
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram()
+{
+    const char *vshaderSource =
+            "void main(void)\n"
+            "gl_Position = vec4(0.0,0.0,0.0,1.0);";
+
+    const char *fshaderSource =
+            "out vec4 color;\n"
+            "void main(void)\n"
+            "{if(gl_FragCoord.x < 200) color = vec4(1.0,0.0,0.0,1.0)); else color = vec4(0.0,0.0,1.0,1.0);}";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1 , &fshaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    GLuint vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+}
+void init(GLFWwindow *window)
+{
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow *window, double currentTime)
 {
-    glClearColor(1.0,0.0,0.0,1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+//    glClearColor(1.0,0.0,0.0,1.0);
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glPointSize(30.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main()
@@ -20,7 +60,7 @@ int main()
 
 //    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 //    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    GLFWwindow *window = glfwCreateWindow(600,800, "Chapter2 - program-1", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(600,600, "Chapter2 - program-1", NULL, NULL);
     glfwMakeContextCurrent(window);
 
     if(glewInit() != GLEW_OK)
