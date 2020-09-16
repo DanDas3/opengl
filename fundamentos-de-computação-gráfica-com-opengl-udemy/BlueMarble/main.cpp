e914523de055ff844447e8119fea00d63d6756d5
Danilo Araújo <daniloaraujo1990@hotmail.com>
2020-09-16 02:27:19 -0300

    Limpando a tela OpenGL e obtendo informações sobre versão e fabricante do OpenGL e Placa Gráfica.

--- fundamentos-de-computação-gráfica-com-opengl-udemy/BlueMarble/main.cpp
+++ fundamentos-de-computação-gráfica-com-opengl-udemy/BlueMarble/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <GL/glew.h>
 #include <glm/glm.hpp>
@@ -6,27 +7,51 @@
 
 using namespace std;
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 int main()
 {
-	glm::vec3 v(1, 2, 3);
-	cout << "Vector: " << glm::to_string(v) << endl;
+	// Inicializa o GLFW
+	assert(glfwInit() == GL_TRUE);
 
-	if (glfwInit())
+	// Criar a janela
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Blue Marble", nullptr, nullptr);
+
+	// Define o contexto ativo do OpenGL
+	glfwMakeContextCurrent(window);
+
+	// Inicializa a GLEW
+	assert(glewInit() == GLEW_OK);
+
+	GLint GLMajorVersion = 0;
+	GLint GLMinorVersion = 0;
+
+	glGetIntegerv(GL_MAJOR_VERSION, &GLMajorVersion);
+	glGetIntegerv(GL_MINOR_VERSION, &GLMinorVersion);
+
+	//cout << "OpenGL version: " << GLMajorVersion << "." << GLMinorVersion << "." << endl;
+
+	// Obtendo informações
+	cout << "OpenGL Vendor	:" << glGetString(GL_VENDOR) << endl;
+	cout << "OpenGL Renderer:" << glGetString(GL_RENDERER) << endl;
+	cout << "OpenGL Version	:" << glGetString(GL_VERSION) << endl;
+	cout << "GLSL Version	:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+
+	// Define a cor padrão ao limpar o buffer de cor
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
 	{
-		cout << "GLFW Initialized with success!" << endl;
-	}
-	else
-	{
-		cout << "GLFW Initialized failed!" << endl;
+		// Limpa o buffer de cor com a cor de fundo predefinida
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
 	}
 
-	if (glewInit() == GLEW_OK)
-	{
-		cout << "GLEW Initialized with success!" << endl;
-	}
-	else
-	{
-		cout << "GLEW Initialized failed!" << endl;
-	}
+	// Encerra o GLFW
+	glfwTerminate();
 	return 0;
 }
